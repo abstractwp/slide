39e541b0c69d0726e2950069edb0acd17debdc9a
DanhThong <danhthong@gmail.com>
2022-06-25 16:40:10 +0700

    refactor: code format

--- src/js/code-editor.js
+++ src/js/code-editor.js
@@ -1,34 +1,37 @@
 const {
-  element: { createElement: e, useRef, useEffect, memo },
-  codeEditor: { initialize, defaultSettings }
+	element: { createElement: e, useRef, useEffect, memo },
+	codeEditor: { initialize, defaultSettings },
 } = window.wp;
 
-export default memo(({ onChange, mode, ...props }) => {
-  const ref = useRef();
+export default memo(
+	( { onChange, mode, ...props } ) => {
+		const ref = useRef();
 
-  useEffect(() => {
-    const editor = initialize(ref.current, {
-      ...defaultSettings,
-      codemirror: {
-        ...defaultSettings.codemirror,
-        tabSize: 2,
-        mode,
-        lineNumbers: false
-      }
-    });
+		useEffect( () => {
+			const editor = initialize( ref.current, {
+				...defaultSettings,
+				codemirror: {
+					...defaultSettings.codemirror,
+					tabSize: 2,
+					mode,
+					lineNumbers: false,
+				},
+			} );
 
-    editor.codemirror.on('change', () => {
-      onChange(editor.codemirror.getValue());
-    });
+			editor.codemirror.on( 'change', () => {
+				onChange( editor.codemirror.getValue() );
+			} );
 
-    return () => {
-      editor.codemirror.toTextArea();
-    };
-  });
+			return () => {
+				editor.codemirror.toTextArea();
+			};
+		} );
 
-  return e('textarea', {
-    ref,
-    ...props
-  });
-  // Never rerender.
-}, () => true);
+		return e( 'textarea', {
+			ref,
+			...props,
+		} );
+		// Never rerender.
+	},
+	() => true
+);
--- src/js/data-subscription.js
+++ src/js/data-subscription.js
@@ -1,21 +1,21 @@
 const {
-  blocks: { createBlock },
-  data: { subscribe, select, dispatch }
+	blocks: { createBlock },
+	data: { subscribe, select, dispatch },
 } = window.wp;
 
-subscribe(() => {
-  const blocks = select('core/block-editor').getBlocks();
-  const block = blocks.find(({ name }) => name !== 'slide/slide');
+subscribe( () => {
+	const blocks = select( 'core/block-editor' ).getBlocks();
+	const block = blocks.find( ( { name } ) => name !== 'slide/slide' );
 
-  if (!block) {
-    return;
-  }
+	if ( ! block ) {
+		return;
+	}
 
-  const slide = createBlock('slide/slide', {}, [
-    block.name === 'core/paragraph'
-      ? createBlock('core/heading')
-      : createBlock(block.name, block.attributes)
-  ]);
+	const slide = createBlock( 'slide/slide', {}, [
+		block.name === 'core/paragraph'
+			? createBlock( 'core/heading' )
+			: createBlock( block.name, block.attributes ),
+	] );
 
-  dispatch('core/block-editor').replaceBlock(block.clientId, slide);
-});
+	dispatch( 'core/block-editor' ).replaceBlock( block.clientId, slide );
+} );
